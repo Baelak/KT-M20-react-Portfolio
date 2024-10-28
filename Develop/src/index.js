9b3f9a7d0e414d1cf45e592b44e4894af93c1264
Baelak <teklemichaelkaleab@gmail.com>
2024-10-27 22:06:20 -0400

    added asset s and code push

--- Develop/src/index.js
+++ Develop/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/App.css';
+import './styles/app.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a root element for rendering
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
