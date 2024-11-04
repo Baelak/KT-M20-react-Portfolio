d7893730061f5b1ff2b6ed4f0bd65f2b30eb29ff
Baelak <teklemichaelkaleab@gmail.com>
2024-11-04 18:20:43 -0500

    build change

--- Develop/vite.config.js
+++ Develop/vite.config.js
@@ -5,14 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   //root: 'public',
-  build: {
-    outDir: 'dist', // Output directory
-    emptyOutDir: true, // Clear previous builds
-    rollupOptions: {
-      external: ['./App.jsx'] // Specify App.jsx as external to prevent resolution issues
-    }
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'] // Recognizes both .js and .jsx
-  }
+ // build: {
+  //  outDir: 'dist', // Output directory
+  //  emptyOutDir: true, // Clear previous builds
+  //  rollupOptions: {
+ //     external: ['./App.jsx'] // Specify App.jsx as external to prevent resolution issues
+ //   }
+//  },
+  //resolve: {
+    //extensions: ['.js', '.jsx'] // Recognizes both .js and .jsx
+ // }
 });
