dff8bdbd2859deaae9da2fe896a5965803b59188
Baelak <teklemichaelkaleab@gmail.com>
2024-11-05 00:28:51 -0500

    Netlify test 5 Portfolio page fix

--- Develop/src/sections/Portfolio.jsx
+++ Develop/src/sections/Portfolio.jsx
@@ -12,28 +12,28 @@ function Portfolio() {
       title: 'Text Editor 🤍',
       deployLink: 'https://kt-m19-text-editor.onrender.com',
       repoLink: 'https://github.com/Baelak/KT-M19-text-Editor',
-      backgroundImage: 'url("../public/assets/images/cards/text-editor-card.png")',
+      backgroundImage: 'url("./assets/images/cards/text-editor-card.png")',
       description: 'A syntax highlighting text editor that can be installed and used offline.'
     },
     {
       title: 'Trivia 🤍',
       deployLink: 'https://baelak.github.io/P1-Trivia/',
       repoLink: 'https://github.com/Baelak/P1-Trivia',
-      backgroundImage: 'url("../public/assets/images/cards/trivia-card.png")',
+      backgroundImage: 'url("./assets/images/cards/trivia-card.png")',
       description: 'DO YOU WANT TO PLAY A GAME? Fun trivia questions to test your knowledge.'
     },
     {
       title: 'Weather Dashboard',
       deployLink: 'https://baelak.github.io/KT-M6-weather-Dashboard/',
       repoLink: 'https://github.com/Baelak/KT-M6-weather-Dashboard',
-      backgroundImage: 'url("../public/assets/images/cards/weather-dashboard-card.png")',
+      backgroundImage: 'url("./assets/images/cards/weather-dashboard-card.png")',
       description: 'Real-time weather updates and forecasts.'
     },
     {
         title: 'Tech Blog',
         deployLink: 'https://kt-m14-tech-blog.onrender.com',
         repoLink: 'https://github.com/Baelak/KT-M14-tech-Blog',
-        backgroundImage: 'url("../public/assets/images/cards/tech-blog-card.png")',
+        backgroundImage: 'url("./assets/images/cards/tech-blog-card.png")',
         description: 'A platform for developers to share and explore tech-related articles.'
      }
 ];
@@ -43,28 +43,28 @@ function Portfolio() {
       title: 'Employee Tracker',
       deployLink: 'https://drive.google.com/file/d/1yKNUc_6bjwE3grIW2DnFPooJcKCb4lyc/view?usp=sharing',
       repoLink: 'https://github.com/Baelak/KT-M12-employee-Tracker',
-      backgroundImage: 'url("../public/assets/images/cards/employee-tracker-card.png")',
+      backgroundImage: 'url("./assets/images/cards/employee-tracker-card.png")',
       description: 'A command-line app for managing employee data.'
     },
     {
       title: 'E-Commerce Back End',
       deployLink: 'https://drive.google.com/file/d/1oA9P6FN421IdUsxls_Ykf_huHuLM8WsU/view?usp=sharing',
       repoLink: 'https://github.com/Baelak/KT-M12-employee-Tracker',
-      backgroundImage: 'url("../public/assets/images/cards/e-commerce-back-end-card.png")',
+      backgroundImage: 'url("./assets/images/cards/e-commerce-back-end-card.png")',
       description: 'A back-end for e-commerce platforms with a SQL database.'
     },
     {
       title: 'Social Network API',
       deployLink: 'https://drive.google.com/file/d/12TP1-jtd9xIAc-h0q6LhpvRQyYGBV4iD/view?usp=drive_link',
       repoLink: 'https://github.com/Baelak/KT-M18-social-Network-API',
-      backgroundImage: 'url("../public/assets/images/cards/social-network-api-card.png")',
+      backgroundImage: 'url("./assets/images/cards/social-network-api-card.png")',
       description: 'API for a social network application using a Mongo database.'
     },
     {
       title: 'SVG Logo Maker',
       deployLink: 'https://drive.google.com/file/d/1aEjNPkRp6DgLnnpQkN7-LeDddfG-Yql0/view?usp=sharing',
       repoLink: 'https://github.com/Baelak/KT-M10-SVG-Logo-Maker',
-      backgroundImage: 'url("../public/assets/images/cards/svg-logo-maker-card.png")',
+      backgroundImage: 'url("./assets/images/cards/svg-logo-maker-card.png")',
       description: 'Easily create customizable SVG logos with a simple CLI application.'
     }
   ];
